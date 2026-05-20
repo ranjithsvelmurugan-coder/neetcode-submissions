16f7cf1b6662dc39b142966bdca249a1e0017153
ranjithsvelmurugan-coder <ranjithsvelmurugan@gmail.com>
2026-05-21 04:19:42 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.java
@@ -0,0 +1,42 @@
+//REMAINDER
+// this solution is not complete stillit runs at o(n^3) have to remove .contains and add other logics 
+
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        int len = nums.length;
+        //Sort the array
+        Arrays.sort(nums);
+
+        for( int i = 0; i < len-2; i++){
+            int target = -nums[i] ;
+            int left = i+1;
+            int right = len - 1 ;
+
+            while(left < right ){
+                int n = nums[left] + nums[right];
+                if( n == target){
+                    List<Integer> tempList = new ArrayList<>();
+                    tempList.add(-target);
+                    tempList.add(nums[left]);
+                    tempList.add(nums[right]);
+                    
+                    if(!list.contains(tempList)){
+                        list.add(tempList);
+                    }
+                    left++;
+                    right--;
+                }
+                else if( n > target){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return list;
+    }
+}
